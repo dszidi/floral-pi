9bd0db19ca7a2cc75b478c0c89af23320cb8b143
dszidi <damianos@knoxbug.org>
2019-11-13 10:28:11 -0500

    Removed rpi-gpio dependency

--- app.js
+++ app.js
@@ -13,26 +13,5 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// GPIO READ EXAMPLE
-var gpio = require('./node_modules/rpi-gpio');
-
-
-// Use rpi pin number scheme
-gpio.setMode(gpio.MODE_BCM);
-
-// Listen
-/*gpio.on('change', function(channel, value) {
-	console.log('Channel ' + channel + ' value is now ' + value);
-});*/
-
-// PIN 4
-gpio.setup(4, gpio.DIR_IN, readInput4);
-function readInput4(err) {
-    if (err) throw err;
-    gpio.read(4, function(err, value) {
-        if (err) throw err;
-        console.log('PIN 4: ' + value);
-    });
-}
 
 
